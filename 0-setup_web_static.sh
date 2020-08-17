93485760e7480ce82f81adf8c510114824ce2fc0
Brett Davis <brett.p.davis101@gmail.com>
2020-08-17 23:22:38 +0000

    Minor content change, added more sudo to be safe

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,10 +4,17 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-touch /data/web_static/releases/test/index.html
-echo "Nginx test content" >> /data/web_static/releases/test/index.html
-ln -fs /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+sudo touch /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 find="^server {$"
 replace="server {\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
 sudo sed -i 's'"${find}"','"${replace}"',' /etc/nginx/sites-available/default
